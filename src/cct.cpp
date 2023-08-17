86235362a9ffcd69f19cd8650d74788b04ed823e
Burak Karamik <burak.karamik@toptal.com>
2023-08-17 20:45:06 +0300

    Added key tracking

--- src/cct.cpp
+++ src/cct.cpp
@@ -7,9 +7,10 @@
 #include <errno.h>
 #include <vector>
 #include <iterator>
-
+#include <algorithm> 
 
 #define MAX_KEY_SIZE 1000
+#define CCT_MODULE_PREFIX "CCT:"
 #define RedisModule_Log(ctx, level, log) std::cout<<"[CCT_MODULE] "<<log<<std::endl
 
 #ifdef __cplusplus
@@ -26,40 +27,57 @@ void Log_Command(RedisModuleString **argv, int argc){
 }
 
 int NotifyCallback(RedisModuleCtx *ctx, int type, const char *event, RedisModuleString *key) {
+    //RedisModule_AutoMemory(ctx);
+
     std::string event_str = event;
     std::string key_str = RedisModule_StringPtrLen(key, NULL);
-    RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "NotifyCallback event : " + event_str  + " , key " + key_str);
-    if (strcasecmp(event, "del") == 0 ||  strcasecmp(event, "set") == 0 ||
-        strcasecmp(event, "expired") == 0 || strcasecmp(event, "evict") == 0 ||
-        strcasecmp(event, "evicted") == 0 || strcasecmp(event, "trimmed") == 0 
-    ) {
-        
+    RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_DEBUG , "NotifyCallback event : " + event_str  + " , key " + key_str);
+
+    // Ignore our self events
+    if (key_str.rfind(CCT_MODULE_PREFIX, 0) == 0){
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_DEBUG , "NotifyCallback event : " + event_str  + " , key " + key_str + " ignore our own events to prevent loops." );
         return REDISMODULE_OK;
     }
 
-    if (strcasecmp(event, "restore") == 0) {
-        
+    // Add prefix
+    std::stringstream prefix_stream;
+    prefix_stream<<CCT_MODULE_PREFIX<<key_str;
+    std::string key_with_prefix = prefix_stream.str(); 
+
+    // Check if the key is tracked
+    RedisModuleCallReply *get_reply = RedisModule_Call(ctx, "GET", "c", key_with_prefix.c_str());
+    if (RedisModule_CallReplyType(get_reply) == REDISMODULE_REPLY_NULL){
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_DEBUG , "NotifyCallback event : " + event_str  + " , key " + key_str + " is not tracked : " + key_with_prefix);
         return REDISMODULE_OK;
+    } else if (RedisModule_CallReplyType(get_reply) != REDISMODULE_REPLY_STRING) {
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "NotifyCallback event : " + event_str  + " , key " + key_str + " getting the stream key failed.");
+        return REDISMODULE_ERR;
+    }
+    RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_DEBUG , "NotifyCallback event : " + event_str  + " , key " + key_str + " is tracked.");
+    RedisModuleString *stream_name = RedisModule_CreateStringFromCallReply(get_reply);
+    
+    RedisModuleString *value;
+    RedisModuleCallReply *json_get_reply = RedisModule_Call(ctx, "JSON.GET", "s", key);
+    if (RedisModule_CallReplyType(json_get_reply) == REDISMODULE_REPLY_NULL){
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "NotifyCallback event : " + event_str  + " , key " + key_str + " getting the value which is gone.");
+        value = RedisModule_CreateString( ctx, "" , 0 );
+    } else if (RedisModule_CallReplyType(json_get_reply) != REDISMODULE_REPLY_STRING) {
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "NotifyCallback event : " + event_str  + " , key " + key_str + " getting the value failed.");
+        return REDISMODULE_ERR;
+    } else {
+        value = RedisModule_CreateStringFromCallReply(json_get_reply);
     }
 
-    if (strcasecmp(event, "rename_from") == 0) { // include also renamenx
-        
-        return REDISMODULE_OK;
-    }
-
-    if (strcasecmp(event, "rename_to") == 0) { // include also renamenx
-        
-        return REDISMODULE_OK;
-    }
-
-    if (strcasecmp(event, "loaded") == 0) {
-        
-        return REDISMODULE_OK;
+    // Write to stream
+    
+    RedisModuleCallReply *xadd_reply =  RedisModule_Call(ctx, "XADD", "sccs", stream_name , "*", key_str.c_str() , value);
+    if (RedisModule_CallReplyType(xadd_reply) != REDISMODULE_REPLY_STRING) {
+            RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "Register_RedisCommand failed to create the stream." );
+            return RedisModule_ReplyWithError(ctx, strerror(errno));
     }
 
     return REDISMODULE_OK;
 }
-
 
 int Register_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     
@@ -74,6 +92,7 @@ int Register_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int arg
     unsigned long long client_id = RedisModule_GetClientId(ctx);
     
     if (RedisModule_SetClientNameById(client_id, argv[1]) != REDISMODULE_OK){
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "Register_RedisCommand failed to set client name." );
         return RedisModule_ReplyWithError(ctx, strerror(errno));
     }
 
@@ -82,7 +101,7 @@ int Register_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int arg
         RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "Register_RedisCommand failed to detect the stream." );
         return RedisModule_ReplyWithError(ctx, strerror(errno));
     }
-    int stream_count = RedisModule_CallReplyInteger(exists_reply);  
+    int stream_count = RedisModule_CallReplyInteger(exists_reply);
 
     if ( stream_count > 0 ){
         RedisModuleCallReply *del_reply = RedisModule_Call(ctx, "DEL", "s", argv[1]);
@@ -92,15 +111,8 @@ int Register_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int arg
         }
     }
     
-    RedisModuleCallReply *xadd_reply =  RedisModule_Call(ctx, "XADD", "sccc", argv[1], "*", "key", "val");
-    if (RedisModule_CallReplyType(xadd_reply) != REDISMODULE_REPLY_STRING) {
-            RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "Register_RedisCommand failed to create the stream." );
-            return RedisModule_ReplyWithError(ctx, strerror(errno));
-    }
-    //RedisModuleCallReply *reply = RedisModule_Call(ctx, "SET", "sl", argv[1], client_id);
-
-    if ( RedisModule_SubscribeToKeyspaceEvents(ctx,REDISMODULE_NOTIFY_GENERIC | REDISMODULE_NOTIFY_SET | REDISMODULE_NOTIFY_STRING |
-            REDISMODULE_NOTIFY_EVICTED | REDISMODULE_NOTIFY_EXPIRED | REDISMODULE_NOTIFY_LOADED ,
+    if ( RedisModule_SubscribeToKeyspaceEvents(ctx, REDISMODULE_NOTIFY_GENERIC | REDISMODULE_NOTIFY_SET | REDISMODULE_NOTIFY_STRING |
+            REDISMODULE_NOTIFY_EVICTED | REDISMODULE_NOTIFY_EXPIRED | REDISMODULE_NOTIFY_LOADED | REDISMODULE_NOTIFY_NEW | REDISMODULE_NOTIFY_MODULE ,
              NotifyCallback) != REDISMODULE_OK ) {
         RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "Register_RedisCommand failed to SubscribeToKeyspaceEvents." );
         return RedisModule_ReplyWithError(ctx, strerror(errno));   
@@ -118,13 +130,11 @@ int FT_Search_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int ar
         return RedisModule_WrongArity(ctx);
     }
     
-    
     Log_Command(argv,argc);
     
-    // Call FT.SEARCH
-    RedisModuleCallReply *reply = RedisModule_Call(ctx, "FT.SEARCH", "v", argv + 1, argc - 1);
+    // Forward Search
+    RedisModuleCallReply *reply = RedisModule_Call(ctx, "FT.SEARCH", "v", argv + 1, argc - 1, "NOCONTENT");
     
-    // Check reply type
     if (RedisModule_CallReplyType(reply) != REDISMODULE_REPLY_ARRAY) {
         return RedisModule_ReplyWithError(ctx, strerror(errno));
     }
@@ -146,6 +156,28 @@ int FT_Search_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int ar
     copy(keys.begin(),keys .end(), std::ostream_iterator<std::string>(s," "));
     RedisModule_Log( ctx, REDISMODULE_LOGLEVEL_DEBUG , "FT.SEARCH returned : " + s.str());
 
+    unsigned long long client_id = RedisModule_GetClientId(ctx);
+    RedisModuleString *client_name = RedisModule_GetClientNameById(ctx, client_id); 
+    if ( client_name == NULL){
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "FT_Search_RedisCommand failed to get client name." );
+        return RedisModule_ReplyWithError(ctx, strerror(errno));
+    }
+    std::string client_name_str = RedisModule_StringPtrLen(client_name, NULL);
+    if ( client_name_str.empty()){
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "FT_Search_RedisCommand failed because client name is not set." );
+        return RedisModule_ReplyWithError(ctx, "Client name is not set or set to empty");
+    }
+
+    for (const auto& it : keys) {
+        std::string key_with_prefix = CCT_MODULE_PREFIX + it; 
+        RedisModuleCallReply *set_reply = RedisModule_Call(ctx, "SET", "cc", key_with_prefix.c_str()  , client_name_str.c_str());
+        if (RedisModule_CallReplyType(set_reply) != REDISMODULE_REPLY_STRING) {
+            RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_WARNING , "FT_Search_RedisCommand failed to add tracked key." );
+            return RedisModule_ReplyWithError(ctx, strerror(errno));
+        }
+    }
+    
+
     return REDISMODULE_OK;
 }
 
@@ -161,10 +193,10 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
         RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_DEBUG , "CCT.REGISTER command created successfully.");
     }
 
-    if (RedisModule_CreateCommand(ctx,"CCT.FT_SEARCH", FT_Search_RedisCommand , "readonly", 0, 0, 0) == REDISMODULE_ERR) {
+    if (RedisModule_CreateCommand(ctx,"CCT.FT.SEARCH", FT_Search_RedisCommand , "readonly", 0, 0, 0) == REDISMODULE_ERR) {
         return REDISMODULE_ERR;
     } else {
-        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_DEBUG , "CCT.FT_SEARCH command created successfully.");
+        RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_DEBUG , "CCT.FT.SEARCH command created successfully.");
     }
     
     return REDISMODULE_OK;
