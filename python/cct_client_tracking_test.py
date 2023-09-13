f9f094deb4bebbcac3c48ed2f7ec37d05387614d
Burak Karamik <burak.karamik@toptal.com>
2023-09-13 16:20:24 +0300

    Update register tests

--- python/cct_client_tracking_test.py
+++ python/cct_client_tracking_test.py
@@ -38,25 +38,19 @@ def test_multi_register_ignored():
     client1 = connect_redis_with_start()
     resp = client1.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_1)
     assert cct_prepare.OK in str(resp)
-    print(resp)
 
     # REGISTER 2. Attempt CLIENT1
-    try:
-        resp = client1.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_1)
-    except redis.exceptions.ResponseError as e:
-        assert cct_prepare.DUPLICATE in str(e)        
+    resp = client1.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_1)
+    assert cct_prepare.OK in str(resp)
 
     # REGISTER CLIENT2
     client2 = connect_redis_with_start()
     resp = client2.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_2)
-    assert cct_prepare.OK in str(resp)
-    print(resp)    
+    assert cct_prepare.OK in str(resp) 
 
     # REGISTER 2. Attempt CLIENT2
-    try:
-        resp = client2.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_2)
-    except redis.exceptions.ResponseError as e:
-        assert cct_prepare.DUPLICATE in str(e)
+    resp = client2.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_2)
+    assert cct_prepare.OK in str(resp)
 
 
 def test_multi_register_client_and_disconnect():
@@ -68,13 +62,11 @@ def test_multi_register_client_and_disconnect():
     client1 = connect_redis_with_start()
     resp = client1.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_1)
     assert cct_prepare.OK in str(resp)
-    print(resp)
 
     # REGISTER CLIENT2
     client2 = connect_redis_with_start()
     resp = client2.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_2)
     assert cct_prepare.OK in str(resp)
-    print(resp)
 
     client1.close()
     client2.close()
@@ -83,4 +75,3 @@ def test_multi_register_client_and_disconnect():
     client3 = connect_redis_with_start()
     resp = client3.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_3)
     assert cct_prepare.OK in str(resp)
-    print(resp)
