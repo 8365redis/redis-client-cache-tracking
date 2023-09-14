4669aa658f086e4ea9da9f8a10ccf679728d8cc8
Burak Karamik <burak.karamik@toptal.com>
2023-09-14 17:26:14 +0300

    Fix expire query

--- src/cct_query_tracking_logic.cpp
+++ src/cct_query_tracking_logic.cpp
@@ -112,10 +112,10 @@ int Handle_Query_Expire(RedisModuleCtx *ctx , std::string key) {
     // FIX HERE : what():  std::out_of_range -> basic_string::substr: __pos (which is 27) > this->size() (which is 22)
     // Handle_Query_Expire parsed  client_name :1 , query :CCT:TRACKED_KEYS:users
     std::string new_query(query.substr(CCT_MODULE_CLIENT_QUERY_PREFIX.length()));
-    new_query = CCT_MODULE_QUERY_PREFIX  + new_query;
-    LOG(ctx, REDISMODULE_LOGLEVEL_WARNING , "Handle_Query_Expire parsed  client_name :" + client_name  + " , new_query :" + new_query);
+    std::string new_query_with_prefix = CCT_MODULE_QUERY_PREFIX  + new_query;
+    LOG(ctx, REDISMODULE_LOGLEVEL_WARNING , "Handle_Query_Expire parsed  client_name :" + client_name  + " , new_query :" + new_query_with_prefix);
 
-    RedisModuleCallReply *srem_key_reply = RedisModule_Call(ctx, "SREM", "cc", new_query.c_str()  , client_name.c_str());
+    RedisModuleCallReply *srem_key_reply = RedisModule_Call(ctx, "SREM", "cc", new_query_with_prefix.c_str()  , client_name.c_str());
     if (RedisModule_CallReplyType(srem_key_reply) != REDISMODULE_REPLY_INTEGER){
         LOG(ctx, REDISMODULE_LOGLEVEL_WARNING , "Query_Track_Check failed while deleting client: " +  client_name);
         return REDISMODULE_ERR;
@@ -125,7 +125,7 @@ int Handle_Query_Expire(RedisModuleCtx *ctx , std::string key) {
     }
 
     // Add event to stream
-    if (Add_Event_To_Stream(ctx, client_name, "query_expired", NULL , "", query) != REDISMODULE_OK) {
+    if (Add_Event_To_Stream(ctx, client_name, "query_expired", NULL , "", new_query) != REDISMODULE_OK) {
         LOG(ctx, REDISMODULE_LOGLEVEL_WARNING , "Query_Track_Check failed to adding to the stream." );
         return REDISMODULE_ERR;
     }
