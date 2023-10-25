7df816aa6641afe45941fa8451983234f930ed56
Burak Karamik <burak.karamik@toptal.com>
2023-10-25 15:30:54 +0300

    Fix for JSON with no root

--- python/cct_updated_key_query_tracking_test.py
+++ python/cct_updated_key_query_tracking_test.py
@@ -3,6 +3,9 @@ from redis.commands.json.path import Path
 from manage_redis import kill_redis, connect_redis_with_start, connect_redis
 import cct_prepare
 from constants import CCT_K2C, CCT_Q_DELI, CCT_EOS
+from redis.commands.json.path import Path
+from redis.commands.search.field import TextField, NumericField, TagField
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 
 @pytest.fixture(autouse=True)
 def before_and_after_test():
@@ -236,3 +239,129 @@ def test_updated_key_match_multiple_queries_one_client():
     # Check key is in streams 
     from_stream = client1.xread( count=2, streams={cct_prepare.TEST_APP_NAME_1:0} )
     assert key_3 in str(from_stream[0][1])
+
+def test_updated_key_match_same_queries_one_client_multi_result_with_base():
+    producer = connect_redis_with_start()
+    cct_prepare.flush_db(producer) # clean all db first
+
+    # TEST SPECIFIC SCHEME
+    TEST_INDEX_NAME = "usersJsonIdx"
+    TEST_INDEX_PREFIX = "users:"
+    schema = (TagField("$.user.a", as_name="user.a"), TagField("$.user.b", as_name="user.b"),  \
+              TagField("$.user.c", as_name="user.c"), TagField("$.user.d", as_name="user.d"))
+    producer.ft(TEST_INDEX_NAME).create_index(schema, definition=IndexDefinition(prefix=[TEST_INDEX_PREFIX], index_type=IndexType.JSON))
+
+    #ADD DATA
+    d = { "user" : {"a" : "a_data" , "b" : "b_data" , "c" : "c_data", "d" : "d_data" } }
+    for i in range(1,20) :
+        key = cct_prepare.TEST_INDEX_PREFIX + str(i)
+        producer.json().set(key, Path.root_path(), d)
+    
+    # SEARCH
+    client1 = connect_redis()
+    client1.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_1)
+    query_value = "a_data"
+    client1.execute_command("CCT.FT.SEARCH "+ cct_prepare.TEST_INDEX_NAME +" @user\\.a:{" + query_value + "}" + " LIMIT 0 100") # match first
+
+    #UPDATE DATA
+    d = { "user" : {"a" : "a_data" , "b" : "b_data2" , "c" : "c_data2", "d" : "d_data2" } }
+    key = cct_prepare.TEST_INDEX_PREFIX + str(10)
+    producer.json().set(key, Path.root_path(), d)
+
+    # Check key is in streams 
+    from_stream = client1.xread( streams={cct_prepare.TEST_APP_NAME_1:0} )
+
+def test_updated_key_match_same_queries_one_client_multi_result():
+    producer = connect_redis_with_start()
+    cct_prepare.flush_db(producer) # clean all db first
+
+    # TEST SPECIFIC SCHEME
+    TEST_INDEX_NAME = "usersJsonIdx"
+    TEST_INDEX_PREFIX = "users:"
+    schema = (TagField("$.a", as_name="a"), TagField("$.b", as_name="b"),  \
+              TagField("$.c", as_name="c"), TagField("$.d", as_name="d"))
+    producer.ft(TEST_INDEX_NAME).create_index(schema, definition=IndexDefinition(prefix=[TEST_INDEX_PREFIX], index_type=IndexType.JSON))
+
+    #ADD DATA
+    d = { "a" : "a_data" , "b" : "b_data" , "c" : "c_data", "d" : "d_data"}
+    for i in range(1,20) :
+        key = cct_prepare.TEST_INDEX_PREFIX + str(i)
+        producer.json().set(key, Path.root_path(), d)
+    
+    # SEARCH
+    client1 = connect_redis_with_start()
+    client1.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_1)
+    query_value = "a_data"
+    client1.execute_command("CCT.FT.SEARCH "+ cct_prepare.TEST_INDEX_NAME +" @a:{" + query_value + "}" + " LIMIT 0 100") # match first
+
+    #UPDATE DATA
+    d = { "a" : "a_data" , "b" : "b_data2" , "c" : "c_data2", "d" : "d_data2"}
+    key = cct_prepare.TEST_INDEX_PREFIX + str(10)
+    producer.json().set(key, Path.root_path(), d)
+
+    # Check key is in streams 
+    from_stream = client1.xread( streams={cct_prepare.TEST_APP_NAME_1:0} )
+
+
+def test_updated_key_match_same_queries_one_client_no_root():
+    producer = connect_redis_with_start()
+    cct_prepare.flush_db(producer) # clean all db first
+
+    # TEST SPECIFIC SCHEME
+    TEST_INDEX_NAME = "usersJsonIdx"
+    TEST_INDEX_PREFIX = "users:"
+    schema = (TagField("$.a", as_name="a"), TagField("$.b", as_name="b"),  \
+              TagField("$.c", as_name="c"), TagField("$.d", as_name="d"))
+    producer.ft(TEST_INDEX_NAME).create_index(schema, definition=IndexDefinition(prefix=[TEST_INDEX_PREFIX], index_type=IndexType.JSON))
+
+    #ADD DATA
+    d = { "a" : "a_data" , "b" : "b_data" , "c" : "c_data", "d" : "d_data"}
+    key = cct_prepare.TEST_INDEX_PREFIX + str(1)
+    producer.json().set(key, Path.root_path(), d)
+    
+    # SEARCH
+    client1 = connect_redis()
+    client1.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_1)
+    query_value = "a_data"
+    client1.execute_command("CCT.FT.SEARCH "+ cct_prepare.TEST_INDEX_NAME +" @a:{" + query_value + "}" + " LIMIT 0 100") # match first
+
+    #UPDATE DATA
+    d = { "a" : "a_data" , "b" : "b_data2" , "c" : "c_data2", "d" : "d_data2"}
+    key = cct_prepare.TEST_INDEX_PREFIX + str(1)
+    producer.json().set(key, Path.root_path(), d)
+
+    # Check key is in streams 
+    from_stream = client1.xread( streams={cct_prepare.TEST_APP_NAME_1:0} )
+
+
+def test_updated_key_match_same_queries_one_client_mixed_data():
+    producer = connect_redis()
+    cct_prepare.flush_db(producer) # clean all db first
+
+    # TEST SPECIFIC SCHEME
+    TEST_INDEX_NAME = "usersJsonIdx"
+    TEST_INDEX_PREFIX = "users:"
+    schema = (TagField("$.a", as_name="a"), TagField("$.b", as_name="b"),  \
+              TagField("$.c", as_name="c"), TagField("$.d.d_1", as_name="d.d_1"))
+    producer.ft(TEST_INDEX_NAME).create_index(schema, definition=IndexDefinition(prefix=[TEST_INDEX_PREFIX], index_type=IndexType.JSON))
+
+    #ADD DATA
+    d = { "a" : "a_data" , "b" : "b_data" , "c" : "c_data", "d" : { "d_1" : "d_1_data" , "d_2" : "d_2_data" }}
+    key = cct_prepare.TEST_INDEX_PREFIX + str(1)
+    producer.json().set(key, Path.root_path(), d)
+    
+    # SEARCH
+    client1 = connect_redis()
+    client1.execute_command("CCT.REGISTER " + cct_prepare.TEST_APP_NAME_1)
+    query_value = "d_1_data"
+    res = client1.execute_command("CCT.FT.SEARCH "+ cct_prepare.TEST_INDEX_NAME +" @d\\.d_1:{" + query_value + "}" + " LIMIT 0 100") # match first
+    print(res)
+
+    #UPDATE DATA
+    d = { "a" : "a_data2" , "b" : "b_data2" , "c" : "c_data2", "d" : { "d_1" : "d_1_data" , "d_2" : "d_2_data2" }}
+    key = cct_prepare.TEST_INDEX_PREFIX + str(1)
+    producer.json().set(key, Path.root_path(), d)
+
+    # Check key is in streams 
+    from_stream = client1.xread( streams={cct_prepare.TEST_APP_NAME_1:0} )
+    print(from_stream)
