b5cd1a936af6ccc840a16779c847c852058d242e
Burak Karamik <burak.karamik@toptal.com>
2023-11-01 18:42:14 +0300

    change include order

--- include/logger.h
+++ include/logger.h
@@ -1,13 +1,15 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include "redismodule.h"
 #include <string>
 #include <string.h>
 #include <chrono>
 #include <iomanip>
 #include <sstream>
 #include <iostream>
+
+#include "redismodule.h"
+
 
 #ifdef _DEBUG
 #define LOG(ctx, level, log) Log_Std_Output(ctx, level, log)
